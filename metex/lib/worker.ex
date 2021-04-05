0c3f610786ba84bef12013ed6e0fec4ec6b9c318
Reginald Braithwaite <reg@braythwayt.com>
2021-04-05 13:55:28 -0400

    WIP on GenServer refactor

---
+++ metex/lib/worker.ex
@@ -0,0 +1,31 @@
+defmodule Metex.Worker do
+  use GenServer
+
+  ## Client API
+
+  def start_link(opts \\ []) do
+    GenServer.start_link(__MODULE__, :ok, opts)
+  end
+
+  def get_temperature(pid, location) do
+    GenServer.call(pid, {:location, location})
+  end
+
+  ## Server API
+
+  def init(:ok) do
+    {:ok, %{}}
+  end
+
+  def handle_call({:location, location}, _from, stats) do
+    case temperature_of(location) do
+      {:ok, temp} ->
+        new_stats = update_stats(stats, location)
+        {:reply, "#{temp} °C", new_stats}
+      _ ->
+        {:reply, :error, stats}
+    end
+  end
+
+  ## Helper Functions
+end
